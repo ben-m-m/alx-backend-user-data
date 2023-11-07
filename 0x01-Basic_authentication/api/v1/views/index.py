7d86666d7bf4289b199dbe7c14890d6b8a69375c
ben-m-m <mainaben23@gmail.com>
2023-11-07 12:59:11 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def authorized() -> str:
     """Raises a 401 error."""
     abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """Raises a 403 error."""
+    abort(403, description="Forbidden")
