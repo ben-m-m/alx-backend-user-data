b6743399d1c35f762d6858ad8a82227cf7438836
ben-m-m <mainaben23@gmail.com>
2023-11-07 13:49:16 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,13 @@ from typing import List, TypeVar
 class Auth:
     """manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """return False"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """returns None - request will be the Flask request object"""
         pass
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """returns None - request will be the Flask request object"""
         pass
