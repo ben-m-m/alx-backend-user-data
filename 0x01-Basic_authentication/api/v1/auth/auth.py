06580cdb8e1926599a237a0ad00775a1fd9dd6ed
ben-m-m <mainaben23@gmail.com>
2023-11-07 14:06:39 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,8 +13,7 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
-        if request is None:
-            return None
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be the Flask request object"""
