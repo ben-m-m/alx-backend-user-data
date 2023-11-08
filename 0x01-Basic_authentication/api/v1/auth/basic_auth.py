9de42963b697987b434e7fde048eecaf3740d292
ben-m-m <mainaben23@gmail.com>
2023-11-08 11:41:32 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+inherits from auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    inherits from auth
+    reverted to from auth
+    """
+    pass
