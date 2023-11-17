df8064cd60dba061ff9bfad36442d4db314ba8bb
ben-m-m <mainaben23@gmail.com>
2023-11-17 13:24:54 +0300

    credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,7 +41,6 @@ class Auth:
             return new_user
         raise ValueError(f'User {email} already exists.')
 
-
     def valid_login(self, email: str, password: str) -> bool:
         """
         expect email and password required arguments and return a boolean.
@@ -51,6 +50,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
         except NoResultFound:
             return False
