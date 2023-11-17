2295e4a58f5162aac475460719e3398e4e1722e5
ben-m-m <mainaben23@gmail.com>
2023-11-17 13:38:44 +0300

    generate UUID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,6 +16,15 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """Generate a new UUID.
+
+    Returns:
+        str: String representation of the generated UUID.
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -55,11 +64,3 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid() -> str:
-        """Generate a new UUID.
-
-    Returns:
-        str: String representation of the generated UUID.
-        """
-        return str(uuid.uuid4())
