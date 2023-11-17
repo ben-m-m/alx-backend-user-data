e51012f780213b781ff840e1a853856bc844891b
ben-m-m <mainaben23@gmail.com>
2023-11-17 10:52:50 +0300

    bcrypt salt hashed passwd

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+ takes in a password string arguments and returns bytes.
+ The returned bytes is a salted hash of the input password,
+ hashed with bcrypt.hashpw
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """salted hash of password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
