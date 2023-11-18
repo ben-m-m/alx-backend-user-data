465af4344bb26ef275b50c8660a3c4485ca47c53
ben-m-m <mainaben23@gmail.com>
2023-11-18 19:24:32 +0300

    update password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -113,7 +113,7 @@ def update_password() -> str:
         reset_token = request.form.get('reset_token')
         new_password = request.form.get('new_password')
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
+        return jsonify({"email": email, "message": "Password updated"})
     except ValueError:
         abort(403)
 
